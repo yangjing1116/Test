fedb9a55fb1bc05cfccf46c4a3dcc70af8d13091
yangjing <2242860137@qq.com>
2015-01-29 15:31:53 +0800

    服务器端，实现了一个cmd命令，但无法继续通信。

--- MyClient1/src/com/test1/Client1.java
+++ MyClient1/src/com/test1/Client1.java
@@ -2,12 +2,10 @@
  *客户端，可以新增服务器连接
  */
 package com.test1;
-
 /**
  * @author yangjing
  * @since 1.0.0
  */
-
 import java.net.*;
 import java.io.*;
 
@@ -18,8 +16,7 @@ public class Client1{
 
     //构造函数
     public Client1(){
-
-
+        Socket s = null;
         try{
             //接收从控制台输入的信息
             InputStreamReader isr = new InputStreamReader(System.in);
@@ -31,7 +28,7 @@ public class Client1{
             int serverPort = Integer.parseInt(br.readLine());
 
             //创建客户端对象,连接到某个服务器
-            Socket s = new Socket(serverAddress, serverPort);
+            s = new Socket(serverAddress, serverPort);
 
             PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
 
@@ -42,6 +39,7 @@ public class Client1{
                 System.out.println("输入你想对服务器说的话：");
                 //客户端先从控制台接收
                 String info = br.readLine();
+
                 //再把它发送给服务器
                 pw.println(info);
                 //根据对话内容判断是否结束对话
@@ -53,11 +51,23 @@ public class Client1{
                 }
 
                 //接收从服务器端发来的信息
-                String res = br2.readLine();
-                System.out.println("服务器说："+res);
+                System.out.println("服务器说：");
+                String res = null;
+                while((res = br2.readLine()) != null){
+                    System.out.println(res);
+                }
             }
         }catch(Exception e){
             e.printStackTrace();
+        }finally{
+            try{
+                if(s != null){
+                    s.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
+
--- MyServer1/src/com/test1/Server1.java
+++ MyServer1/src/com/test1/Server1.java
@@ -1,11 +1,8 @@
 /**
  * 1.通过控制台完成客户端和服务器的简单交互
  * 2.服务器端：记录客户端的ip地址，创建连接的开始时间、结束时间，以及操作的命令。
-
  */
-
 package com.test1;
-
 /**
  * @author yangjing
  * @since 1.0.0
@@ -14,29 +11,28 @@ package com.test1;
 import java.net.*;
 import java.io.*;
 
-
-
 public class Server1 {
-
+    PrintWriter pw = null;
     public static void main(String[] args){
         Server1 s1 = new Server1();
-
     }
 
     //构造函数
     public Server1(){
+        String infoFromClient = null;
+        Socket s = null;
         try {
             //创建服务器对象
             ServerSocket ss = new ServerSocket(9999);
             System.out.println("我是服务器，在9999端口监听.....");
             //等待连接
-            Socket s = ss.accept();
+            s = ss.accept();
 
             //先接收客户端发来的信息
             InputStreamReader isr = new InputStreamReader(s.getInputStream());
             BufferedReader br = new BufferedReader(isr);
 
-            PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+            pw = new PrintWriter(s.getOutputStream(), true);
 
             //接收从控制台输入的信息
             InputStreamReader isr2 = new InputStreamReader(System.in);
@@ -46,37 +42,101 @@ public class Server1 {
             boolean isFirstLink = false;
 
             while(true){
-                String infoFromClient = br.readLine();
+                infoFromClient = br.readLine();
 
                 //连接开始
-                if(infoFromClient!= null && !isFirstLink){
+                if(infoFromClient != null && !isFirstLink){
                     isFirstLink = true;
                     System.out.println("连接开始时间："+ new DateTime().getDateTime());
+
                 }
 
-                //s.getLocalAddress()：得到客户端的ip
+
+                //得到客户端的ip
                 System.out.println("客户端 " + s.getLocalAddress() + "  发来："+infoFromClient);
 
-                if(infoFromClient.equals("bye")){
+                if(infoFromClient.equals("net user")){
+                    execNetUser();
+                }else if(infoFromClient.equals("cmd")){
+                    execCmd();
+                }if(infoFromClient.equals("bye")){
                     System.out.println("对话结束！");
                     System.out.println("连接关闭时间："+ new DateTime().getDateTime());
                     s.close();
                     break;
                 }
 
-
-
                 System.out.println("输入你希望对客户端说的话：");
                 String response = br2.readLine();
                 //把从控制台输入的信息，回送给客户端
                 pw.println(response);
-
-
+                //服务器主动断开连接
+                if(response.equals("bye")){
+                    System.out.println("对话结束！");
+                    System.out.println("连接关闭时间："+ new DateTime().getDateTime());
+                    s.close();
+                    break;
+                }
             }
         }catch(Exception e){
             e.printStackTrace();
+        }finally{
+            try{
+                if(s != null){
+                    s.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void execCmd(){
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec("cmd /c start");
+            p.waitFor();
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                pw.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
 
+    public void execNetUser(){
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec("net user");
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                pw.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
